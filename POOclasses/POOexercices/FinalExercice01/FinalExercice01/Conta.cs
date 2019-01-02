278a681a80361ba94043bf7c8014a389ee83a4fd
guih2127 <guih2127@gmail.com>
2019-01-02 00:05:09 -0200

    final exercice about the first concepts

---
+++ POOclasses/POOexercices/FinalExercice01/FinalExercice01/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExercice01
+{
+    class Conta
+    {
+        private int _numero;
+        public double Saldo { get; private set; }
+        public string Nome { get; private set; }
+
+        public Conta(int numero, string nome)
+        {
+            _numero = numero;
+            Nome = nome;
+            Saldo = 0;
+        }
+
+        public Conta(double valor, int numero, string nome) : this(numero, nome)
+        {
+            Saldo = valor;
+        }
+
+        public int Numero
+        {
+            get { return _numero; }
+            set
+            {
+                if (_numero == 0)
+                {
+                    _numero = value;
+                }
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo = Saldo - (valor + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Numero: {_numero}, Saldo: {Saldo}";
+        }
+    }
+}
---
+++ POOclasses/POOexercices/FinalExercice01/FinalExercice01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+
+            Console.WriteLine("Vamos criar a sua conta! Deseja fazer um depósito inicial? Digite 1 se Sim ou outro número se não.");
+            int escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1)
+            {
+                Console.Write("Informe o número da conta: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Agora, informe o valor do depósito inicial: ");
+                double valor = double.Parse(Console.ReadLine());
+
+                c = new Conta(valor, numero, nome);
+            }
+
+            else
+            {
+                Console.Write("Informe o número da conta: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe seu nome: ");
+                string nome = Console.ReadLine();
+
+                c = new Conta(numero, nome);
+            }
+
+            Console.WriteLine(c);
+
+            Console.Write("Agora, vamos fazer um depósito, digite o valor: ");
+            double valorDep = double.Parse(Console.ReadLine());
+            c.Deposito(valorDep);
+            Console.WriteLine(c);
+
+            Console.Write("Agora, vamos fazer um saque, digite o valor: ");
+            double valorSaq = double.Parse(Console.ReadLine());
+            c.Saque(valorSaq);
+            Console.WriteLine(c);
+        }
+    }
+}
